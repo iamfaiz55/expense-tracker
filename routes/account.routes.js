e099e7fbda435255a276d76058dbec39c35e7fe9
john <you@example.com>
2024-06-10 13:21:21 +0530

    initial commit

---
+++ routes/account.routes.js
@@ -0,0 +1,9 @@
+const { credit, debit } = require("../controllers/account.controller")
+
+const routes = require("express").Router()
+
+routes 
+    .post("/credit", credit)
+    .post("/debit", debit)
+
+module.exports = routes
